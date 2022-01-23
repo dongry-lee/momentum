1b8e5fe3deacdd3ffc4c191c9a2e4e25e1981c12
dongry-lee <89010847+dongry-lee@users.noreply.github.com>
2022-01-24 02:49:01 +0900

    js clone coding

--- js/quotes.js
+++ js/quotes.js
@@ -1,14 +1,50 @@
 const qoutes = [
-  { qoute: "1", autor: "1" },
-  { qoute: "2", autor: "2" },
-  { qoute: "3", autor: "3" },
-  { qoute: "4", autor: "4" },
-  { qoute: "5", autor: "5" },
-  { qoute: "6", autor: "6" },
-  { qoute: "7", autor: "7" },
-  { qoute: "8", autor: "8" },
-  { qoute: "9", autor: "9" },
-  { qoute: "10", autor: "10" },
+  {
+    qoute:
+      "The road to success and the road to failure are almost exactly the same.",
+    autor: "Colin R. Davis",
+  },
+  {
+    qoute: "It is better to fail in originality than to succeed in imitation.",
+    autor: "Herman Melville",
+  },
+  {
+    qoute:
+      "Success is walking from failure to failure with no loss of enthusiasm.",
+    autor: "Winston Churchill",
+  },
+  {
+    qoute: "All progress takes place outside the comfort zone.",
+    autor: "Michael John Bobak",
+  },
+  {
+    qoute:
+      "Success usually comes to those who are too busy to be looking for it.",
+    autor: "Henry David Thoreau",
+  },
+  {
+    qoute: "The way to get started is to quit talking and begin doing.",
+    autor: "Walt Disney",
+  },
+  {
+    qoute:
+      "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    autor: "Albert Schweitzer",
+  },
+  {
+    qoute:
+      "Success seems to be connected with action. Successful people keep moving.",
+    autor: "Conrad Hilton",
+  },
+  {
+    qoute: "In order to succeed, we must first believe that we can.",
+    autor: "Nikos Kazantzakis",
+  },
+  {
+    qoute:
+      "The only place where success comes before work is in the dictionary.",
+    autor: "Vidal Sassoon",
+  },
 ];
 
 const qoute = document.querySelector("#qoute span:first-child");
